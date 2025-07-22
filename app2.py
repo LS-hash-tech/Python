7edf2da69efd58e2d06af64c261a75b952adb02e
Layne Singh <singa0906@icloud.com>
2025-07-22 01:36:48 +0100

    solve problems

--- app2.py
+++ app2.py
@@ -35,3 +35,32 @@ months = 12
 # 3. Subtract 1 to get the monthly growth rate
 cmgr = math.pow(final / initial, 1 / months) - 1
 print(f"Compound Monthly Growth Rate: {cmgr * 100:.2f}%")
+
+# startup valued at £5 million. You’re growing at a consistent monthly rate of 8%.
+# How many months will it take for your valuation to double?
+
+val_initial = 5000000
+val_final = 10000000
+Monthly_growth = 8  # as it is a percent you need to divide by 100
+
+print(Monthly_growth / 100 + 1)
+# valuation is growing by 8% means monthly is growing 108%
+# Means each month, the new value is 108% of the previous one
+Monthly_growth = 1.08
+
+value = 5000000
+growth_rate = 0.08
+value *= (1 + growth_rate)  # Same as value *= 1.08
+
+print(5000000 * 1.08)  # M1 = 5400000
+print(5400000 * 1.08)  # M2 = 5832000
+print(5832000 * 1.08)  # M3 = 6298560
+print(6298560 * 1.08)  # M4 = 6802444.800000001
+print(6802444.800000001 * 1.08)  # M5 = 7346640.3840000015
+print(7346640.3840000015 * 1.08)  # M6 = 7934371.61
+print(7934371.61 * 1.08)  # M7 = 8569121.33
+print(8569121.33 * 1.08)  # M8 = 9254651.0364
+print(9254651.0364 * 1.08)  # M9 = 9995023.11
+print(round(9995023.11))
+
+print(f"\nIt took {9} months to double the valuation.")  # M9 = 9995023.11
