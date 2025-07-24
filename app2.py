9eeab1df9a750dea2b6450e172d8f9a631e87ef8
Layne Singh <singa0906@icloud.com>
2025-07-24 22:29:03 +0100

    updates and more learning

--- app2.py
+++ app2.py
@@ -76,6 +76,177 @@ months = 18
 users_remaining = 50000
 
 # calculate compound monthly churn
+# 1 through 18
+# y = x + 1
+# built in functions:
+# int(x)
+# float(x)
+# bool(x)
+# str(x)
 
-for months in range(1, 19):  # 1 through 18
-print(f"months {months}: {users_initial} users")
+Temperature = 15
+if Temperature > 30:
+    print("it's warm")
+    print("You can drink the water!")
+elif Temperature > 20:
+    print("it's alright")
+    print("Thanks, will do")
+else:
+    print("it's cold")
+print("done")
+
+# method 1 of running the below
+age = 22
+if age >= 18:  # >= equal to greater than or equal to (eg. age restriction)
+    print("eligible")
+else:
+    print("not eligible")
+
+# method 2 of running it (cleaner version also known as ternary operator)
+age = 18
+message = "eligible" if age >= 18 else "not eligible"
+print(message)
+
+# =========================== logical operators ===============================
+
+# and
+# or
+# not
+
+# use case below
+
+high_income = True
+good_credit = True
+home_owner = False
+
+if high_income and good_credit:
+    print("eligible")
+if home_owner is False:
+    print("not eligible")
+
+# 2nd use case (self scripted)
+
+working_keyboard = True
+Clean_exterior = False
+Intact_pixel = True
+functioning_speakers = False
+
+if working_keyboard and Intact_pixel and functioning_speakers is True:
+    print("eligible")
+else:
+    print("not eligible")
+
+# 3rd use case (self scripted)
+
+student_id = True
+Uniform = False
+
+if student_id or Uniform is not True:
+    print("Not eligible to enter in class")
+else:
+    print("welcome to the lecture")
+
+# 4th use case (self scripted)
+
+new_skateboard = True
+new_wheels = False
+dented_front = True
+
+if not dented_front:
+    print("open for negotiation")
+else:
+    print("not interested")
+
+# 2nd eg of 4th use
+if (dented_front or new_wheels) and not new_skateboard:
+    print("open for negotiation")
+else:
+    print("not interested")
+
+# ======================== chaining comparison operator =========================
+
+age = 67
+
+if age >= 65 and age < 120:
+    print("age not suitable")
+else:
+    print("age suitable")
+
+# the same can be written neater (as per below)
+
+age = 67
+
+if 65 <= age < 120:
+    print("not suitable x 2")
+else:
+    print("age is suiable x 2")
+
+# ================================== Loops ====================================
+
+# loops are great if you want to repeat a process over and over again
+
+for number in range(10):
+    print("attempt", number)
+
+# 2nd use case
+
+for number in range(10):
+    print("attempt", number + 1, (number + 1) * ".")
+
+    # the outcome is the following:
+    # attempt 1 .
+    # attempt 2 ..
+    # attempt 3 ...
+    # and so on................
+
+# code simplification
+
+for number in range(1, 10):  # <---- same result but cleaner code
+    print("attempt", number, (number) * ".")
+
+# or
+
+for number in range(1, 10, 2):  # last number means "skip by 2 until 10 is reached"
+    print("attempt", number, (number) * ".")
+
+# Use case (self scripted)
+
+for number in range(200, 299, 11):
+    # * is used to keep number & dots aligned
+    print("shot number", number, (number) * ".")
+
+# ================================== For.. Else ====================================
+
+successful = True
+
+for number in range(1, 100):
+    print("Email attempted")
+    if successful:
+        print("sucessful")
+        break  # this will stop lines from being executed 100 times
+
+# what if the attempt was failed and we want to show a different message now?
+
+successful = False
+
+for number in range(1, 100):
+    print("Email attempted")
+    if successful:
+        print("sucessful")
+        break
+else:    # executed if the loop doesn't reach "break" but runs 100 times
+    print("attempted 100 times and failed")
+    # eg:
+    # Email attempted
+    # Email attempted
+    # and so on for another 98 times......
+
+# ================================= Nested Loops ==================================
+
+# Essentially one loop inside another loop
+
+for x in range(5):
+    for y in range(3):
+        print(f"({x}, {y})")
+
+# Check personal notes for full breakdown
