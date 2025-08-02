00b6b9bb93859781f79eadba6872ed0e33f413bc
Layne Singh <singa0906@icloud.com>
2025-08-02 21:54:54 +0100

    start of project 1

--- projects with py/project.py
+++ projects with py/project.py
@@ -1,6 +1,6 @@
 # ======================================== dice roll game ======================================
 #
-# functions and structure
+# PERSONAL TEST: functions and structure
 
 # ask: roll the dice? (Y/N)
 
@@ -9,12 +9,23 @@
 # Y = two random numbers in parenthesis for two dices -- eg. (2, 5) -- each dice can offer from 1 to 6
 # Anything else? = Invalid choice!
 
+import random
+Y = (1, 2, 3, 4, 5, 6)
+N = "Thank you for playing and have a great rest of your day"
+
 for numbers in range(1, 6):
-    if Y
-    print("{numbers}")
-    elif:
-        N print("Thank you for playing and have a great rest of your day")
+    if Y:
+        print("{numbers}")
+    elif N:
+        print("Thank you for playing and have a great rest of your day")
     else:
         print("Sorry, invalid choice!")
 
 Numbers = (1, 2, 3, 4, 5, 6)
+
+# The outcome for this Personal attempt came out with Numbers x 5
+
+# Attempt 2 (with some inside info!)
+
+
+choice = input
