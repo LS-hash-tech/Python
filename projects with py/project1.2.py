62d92aa2e70a8b460ae1f39431b0ba91f2b50bfc
Layne Singh <singa0906@icloud.com>
2025-08-04 14:57:26 +0100

    small update

--- projects with py/project1.2.py
+++ projects with py/project1.2.py
@@ -1,19 +1,21 @@
 import random
 
-choose = input("To start - how many dices would you like to use? (1 or 2):")
+choose = int(
+    input("To start - how many dices would you like to use? (1 or 2):"))
+1 == 1
+while True:
+    choice = input("roll the dice? (y/n): ")
+    if choice == 'y':
+        dice1 = random.randint(1, 6)
+        print(f"({dice1})")
+    elif choice == 'n':
+        print("thank you for playing, have a great rest of your day")
+    else:
+        print('invalid answer, please choose between "y" or "n"')
 
-if choose == 1:
-    while True:
-        choice = input("roll the dice? (y/n): ")
-        if choice == 'y':
-            dice1 = random.randint(1, 6)
-            print(f"({dice1})")
-        elif choice == 'n':
-            print("thank you for playing, have a great rest of your day")
-        else:
-            print('invalid answer, please choose between "y" or "n"')
 
 elif choose == 2:
+    2 = 2
     while True:
         choice = input("roll the dice? (y/n): ")
         if choice == 'y':
